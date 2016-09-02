3f0a73dc8d2ea6b0b8704b6fcaa5bad2a8ec59f7
ArteneR <georgevici.ioan@yahoo.com>
2016-09-02 21:43:41 +0300

    Changed flags style from rectangle to round

--- public/js/language-select.js
+++ public/js/language-select.js
@@ -1,10 +1,5 @@
 $(document).ready(function() {
 
-    $("#language-flag, #all-languages").click(function() {
-        $("#all-languages").toggle("display");
-        $("#language-flag-gray-filter").toggle("display");
-    });
-
     $("#all-languages > div").click(function() {
         var selected_language = $(this).attr('value');
         select_language(selected_language);
--- public/js/main.js
+++ public/js/main.js
@@ -4,18 +4,6 @@
 
         translatePage();
 
-        $('body').click(function(e) {
-            hideLanguagesIfClickedOutside(e);
-        });
-
     });
 
 })(this);
-
-
-function hideLanguagesIfClickedOutside(e) {
-    if ($("#language-select").has(e.target).length === 0) {
-        $("#all-languages").css('display', 'none');
-        $("#language-flag-gray-filter").css('display', 'none');
-    }
-}
